ab8455aabb23f5c4ce817e14aac6b578883dc853
soren <soren@two-x.com>
2023-11-21 19:46:35 -0500

    debugged, this works now.

--- src/scavenge.cpp
+++ src/scavenge.cpp
@@ -8,29 +8,28 @@
 // Truncate value to one decimal
 // Optional:  that you did it correctly
 
-static const int composites[26] = { 4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39 };
-char alphabet[26];
-
-int shifted[26];
-float divided[26];
-float multiplied[26];
-float truncated[26];
-char letter[26];
 int age = 9;
+int cumposit[26] = { 4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39 };
+String alpha[26];
+int shifted[26];
+float divd[26];
+float mult[26];
+float truncd[26];
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
-    for (int i=0; i<26; i++) alphabet[i] = (char)(0x41 + i);
+    for (int i=0; i<26; i++) alpha[i] = String((char)('A' + i));
 }
 void loop() {
     printf("\n\nPsn Lt Cmp Shf  Div     Mult   Trunc Lt\n");
     for (int i=0; i<26; i++) {        
-        shifted[i] = composites[(i + age) % 26];
-        divided[i] = (float)(i + 1) / (float)shifted[i];
-        multiplied[i] = 10.0 * divided[i];
-        truncated[i] = (float)((int)(10.0 * (multiplied[i] + 0.001)) / 10.0);
-        printf("%2d  %s  %2d  %2d  %6.4f  %6.3f  %4.1f  %s\n", i+1, String(alphabet[i]), composites[i], shifted[i], divided[i], multiplied[i], truncated[i], String(alphabet[i])); 
+        shifted[i] = cumposit[(i + age) % 26];
+        divd[i] = (float)(i + 1) / (float)shifted[i];
+        mult[i] = 10.0 * divd[i];
+        truncd[i] = (float)((int)(10.0 * (mult[i] + 0.001)) / 10.0);
+        printf("%2d  %s  %2d  %2d  %6.4f  %6.3f  %4.1f  %s\n", i+1, alpha[i],
+            cumposit[i], shifted[i], divd[i], mult[i], truncd[i], alpha[i]); 
     }
     while(true);
 }
